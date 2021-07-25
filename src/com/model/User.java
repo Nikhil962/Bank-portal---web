bf9396d2f3c69bf81056de3c50d4a15cc81c3403
Nikhil962 <54204556+Nikhil962@users.noreply.github.com>
2021-07-25 14:22:57 +0530

    Create User.java

---
+++ src/com/model/User.java
@@ -0,0 +1,53 @@
+package com.model;
+
+public class User {
+	private String name;
+	private String dof;
+	private String bankaccountno;
+	private String ifsecode;
+	private String mobileno;
+	private String amount;
+	private String pass;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDof() {
+		return dof;
+	}
+	public void setDof(String dof) {
+		this.dof = dof;
+	}
+	public String getBankaccountno() {
+		return bankaccountno;
+	}
+	public void setBankaccountno(String bankaccountno) {
+		this.bankaccountno = bankaccountno;
+	}
+	public String getIfsecode() {
+		return ifsecode;
+	}
+	public void setIfsecode(String ifsecode) {
+		this.ifsecode = ifsecode;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+}
